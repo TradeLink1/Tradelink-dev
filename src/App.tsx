37a9fdc0542cfbeb076ecae2dd222525fe6d3145
Ishola2283 <olamilekan2283@gmail.com>
2025-08-06 16:05:29 +0100

    ishola homepage design

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,38 @@
-import Header from "./static/Header"
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Home from "./pages/Homepage/Home"
-// import Categories from "./pages/Categories/Categories"
-import AddBusiness from "./pages/AddBusiness/AddBusiness"
-import Faq from "./pages/Faq/Faq"
-import Contact from "./pages/Contact/Contact"
-import Login from "./pages/Login/Login"
-import Register from "./pages/Register/Register"
-import Product from "./pages/Categories/Product"
-import Services from "./pages/Categories/Services"
-import Footer from "./static/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./static/Header";
+import Footer from "./static/Footer";
+import Home from "./pages/Homepage/Home";
+// import Categories from "./pages/Categories/Categories";
+import SellWithUs from "./pages/SellWIthUs/SellWithUs";
+import Faq from "./pages/Faq/Faq";
+import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Product from "./pages/Categories/Product";
+import Services from "./pages/Categories/Services";
+import { SearchProvider } from "./context/SearchContext";
 
 const App = () => {
   return (
-    <div>
+    <SearchProvider>
       <BrowserRouter>
-            <Header/>
-            
+        <Header />
+
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          {/* <Route path="/Categories" element={<Categories/>}/> */}
-          <Route path="/Categories/Products" element={<Product/>}/>
-          <Route path="/Categories/Services" element={<Services/>}/>
-          <Route path="/AddBusiness" element={< AddBusiness/>}/>
-          <Route path="/Faq" element={< Faq/>}/>
-          <Route path="/Contact" element={<Contact/>}/> 
-          <Route path="/Login" element={<Login/>}/>
-          <Route path="Register" element={<Register/>}/>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/Categories" element={<Categories />} /> */}
+          <Route path="/Categories/Products" element={<Product />} />
+          <Route path="/Categories/Services" element={<Services />} />
+          <Route path="/SellWithUs" element={<SellWithUs />} />
+          <Route path="/Faq" element={<Faq />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
         </Routes>
-         <Footer/>
+        <Footer />
       </BrowserRouter>
+    </SearchProvider>
+  );
+};
 
-    
-    </div>
-  )
-}
-
-export default App
+export default App;
